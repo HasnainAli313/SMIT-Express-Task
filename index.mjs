9ed5a092e12583e0763e23e3dde17626b12e688c
Hasnain Ali <93475112+HasnainAli313@users.noreply.github.com>
2024-09-21 22:43:31 +0500

    added get request

--- index.mjs
+++ index.mjs
@@ -21,5 +21,10 @@ let items = [
 app.get('/api/items', (req, res) => {
     res.status(200).json({ message: 'GET request - Fetching all items', data: items });
   });
-  
 
+
+app.post('/api/items', (req, res) => {
+  const newItem = req.body;
+  items.push(newItem);
+  res.status(201).json({ message: 'POST request - Adding new item', data: newItem });
+});
